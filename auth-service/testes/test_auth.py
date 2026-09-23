d02aecf33e430484ff0cc4ec07d7d8c9592fdf24
Douglas <seu-email-do-github@exemplo.com>
2026-09-22 21:46:48 -0300

    adiciona testes falta fazer rodar

--- auth-service/testes/test_auth.py
+++ auth-service/testes/test_auth.py
@@ -3,7 +3,7 @@ from app.main import app
 
 client = TestClient(app)
 
-def test_health_check():
+def test_status_check():
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"status": "healthy", "service": "auth-service"}
@@ -11,4 +11,9 @@ def test_health_check():
 def test_login_sucesso():
     response = client.post("/auth/login", json={"id": "admin", "password": "admin"})
     assert response.status_code == 200
+    assert "access_token" in response.json()
+
+def test_login_alha():
+    response = client.post("/auth/login", json={"id": "admin1", "password": "admin1"})
+    assert response.status_code == 401 #verifica se deu erro 
     assert "access_token" in response.json()
